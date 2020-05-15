24faa1d45e45d680b72311c345902daa92fcbcde
Joshua Shaw <joshua@joshuas-mbp.lan>
2020-05-15 17:19:42 -0600

    Created 2 components and login/auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { Switch, Link } from 'react-router-dom'
+import BubblePage from './components/BubblePage'
 import Login from "./components/Login";
 import "./styles.scss";
+import PrivateRoute from './components/PrivateRoute';
+import ColorList from './components/ColorList'
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <Switch>
+          <PrivateRoute exact path='/' component={BubblePage} />
+        </Switch>
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,57 @@
-import React from "react";
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+const Login = (props) => {
+    const [newUser, setUser] = useState({
+        username: '',
+        password: ''
+    })
 
-export default Login;
+    const handleChange = (e) => {
+        setUser({ ...newUser, [e.target.name]: e.target.value })
+
+        console.log(newUser)
+    }
+
+    const login = (e) => {
+        e.preventDefault()
+        console.log(newUser)
+
+        axios   
+            .post('http://localhost:5000/api/login', newUser)
+            .then(res => {
+              localStorage.setItem('token', res.data.payload);
+              console.log(res.data.payload);
+              props.history.push('/')
+            })
+            .catch(err => {
+              console.log(err);
+            })
+    }
+    return (
+        <div>
+            <form onSubmit={e => login(e)}>
+              <div>Username:</div>
+                <input
+                  type="text"
+                  name="username"
+                  onChange={e => handleChange(e)}
+                />
+                <br></br>
+                <div>Password:</div>
+                <input
+                  type='text'
+                  name='password'
+                  onChange={e => handleChange(e)}
+                />
+                <br></br>
+                <button
+                   type='submit'
+                >Login</button>
+            </form>
+        </div>
+      )
+  }
+  
+  export default Login;
